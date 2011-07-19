fa0159619541a2aeb7fe8e9e776ae65e13c56ab5
Scott Conley <scott@14to9.org>
2011-07-19 16:55:44 -0400

    builder log level to debug

--- config/environments/builder.rb
+++ config/environments/builder.rb
@@ -1,6 +1,6 @@
 config.cache_classes = true
 config.log_path = CRUISE_OPTIONS[:log_file_name] || 'log/builder_WITHOUT_A_NAME.log'
-config.log_level = CRUISE_OPTIONS[:verbose] ? :debug : :info
+config.log_level :debug
 
 config.after_initialize do
   require CRUISE_DATA_ROOT + '/site_config' if File.exists?(CRUISE_DATA_ROOT + "/site_config.rb")
